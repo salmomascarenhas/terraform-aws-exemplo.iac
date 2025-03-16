a06e6104172762f536c7abcee4bc462770368598
salmomascarenhas <salmo.cruz@gmail.com>
2025-03-15 23:19:29 -0300

    Add variables for main bucket name and AWS profile name

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_main" {
+    type = string
+    description = "The name of the bucket"
+}
+
+variable "aws_profile_name" {
+    type = string
+    description = "The name of the profile"
+}
