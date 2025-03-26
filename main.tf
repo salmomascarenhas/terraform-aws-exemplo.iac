4d772c5c377e5c57090a12e923199416845ff611
salmomascarenhas <salmo.cruz@gmail.com>
2025-03-26 10:59:28 -0300

    Format module configurations for S3 and CloudFront for improved readability

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "s3" {
-    source = "./modules/s3"
-    s3_bucket_name = "bucket-s3-iac"
+  source         = "./modules/s3"
+  s3_bucket_name = "bucket-s3-iac"
 }
 
 module "cloudfront" {
-    source = "./modules/cloudfront"
-    origin_id = module.s3.bucket_id
-    bucket_domain_name = module.s3.bucket_domain_name
-    depends_on = [module.s3]
+  source             = "./modules/cloudfront"
+  origin_id          = module.s3.bucket_id
+  bucket_domain_name = module.s3.bucket_domain_name
+  depends_on         = [module.s3]
 }
